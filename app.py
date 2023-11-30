8121a3f9261bb8ee069778209f902c19a315f8c0
José-Gilberto <92352192+Gil987654321@users.noreply.github.com>
2023-11-30 02:22:22 +0000

    Crear la lógica del juego

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from flask import Flask, render_template, request
+import random
+import os
 
-from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+# Lista de opciones: piedra, papel, tijeras
+opciones = ['piedra', 'papel', 'tijeras']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/jugar', methods=['POST'])
+def jugar():
+    eleccion_jugador = request.form['eleccion'].lower()
+
+    if eleccion_jugador not in opciones:
+        return "Opción no válida. Por favor, elige entre piedra, papel o tijeras."
+
+    eleccion_oponente = random.choice(opciones)
+    resultado = determinar_ganador(eleccion_jugador, eleccion_oponente)
+
+    return render_template('resultado.html', eleccion_jugador=eleccion_jugador, eleccion_oponente=eleccion_oponente, resultado=resultado)
+
+def determinar_ganador(jugador, oponente):
+    # Lógica para determinar el ganador
+    if jugador == oponente:
+        return "Empate"
+    elif (jugador == 'piedra' and oponente == 'tijeras') or (jugador == 'tijeras' and oponente == 'papel') or (jugador == 'papel' and oponente == 'piedra'):
+        return "¡Ganaste!"
+    else:
+        return "¡Perdiste!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
